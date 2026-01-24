9a7eb72d355d4893aba78aa9c39876d5558805c3
HITESHVERMA01 <vermahitesh419@gmail.com>
2026-01-24 11:29:53 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,15 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://localhost:5000", // 🔴 CHANGE if backend uses 8000
         changeOrigin: true,
-      },
-      "/video_feed": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
-  },
+        secure: false
+      }
+    }
+  }
 });
